c616105c003c6c8cd1740bae1386c3783ff21ce8
Maxine Alexander <25956545+MaxTheMooshroom@users.noreply.github.com>
2025-05-06 15:43:34 -0400

    add tundra to core targets, more updates to tundra target infra

--- llvm/lib/Target/Tundra/TargetInfo/TundraTargetInfo.cpp
+++ llvm/lib/Target/Tundra/TargetInfo/TundraTargetInfo.cpp
@@ -1,1 +1,21 @@
+//===-- TundraTargetInfo.h - Tundra Target Implementation -------*- C++ -*-===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
 
+#include "TargetInfo/TundraTargetInfo.h"
+#include "llvm/MC/TargetRegistry.h"
+using namespace llvm;
+
+Target &llvm::getTheTundraTarget() {
+  static Target TheTundraTarget;
+  return TheTundraTarget;
+}
+
+extern "C" LLVM_EXTERNAL_VISIBILITY void LLVMInitializeTundraTargetInfo() {
+  RegisterTarget<Triple::tundra>
+      X(getTheTundraTarget(), "tundra", "Tundra (16-bit little endian)", "Tundra");
+}
--- llvm/lib/Target/Tundra/TargetInfo/TundraTargetInfo.h
+++ llvm/lib/Target/Tundra/TargetInfo/TundraTargetInfo.h
@@ -1,1 +1,20 @@
+//===-- TundraTargetInfo.h - Tundra Target Implementation -------*- C++ -*-===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
 
+#ifndef LLVM_LIB_TARGET_TUNDRA_TARGETINFO_TUNDRATARGETINFO_H
+#define LLVM_LIB_TARGET_TUNDRA_TARGETINFO_TUNDRATARGETINFO_H
+
+namespace llvm {
+
+class Target;
+
+Target &getTheTundraTarget();
+
+} // namespace llvm
+
+#endif // LLVM_LIB_TARGET_TUNDRA_TARGETINFO_TUNDRATARGETINFO_H
